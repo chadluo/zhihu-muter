fcbf490468cb9ad9eacdbc0cef1c37c1c4847e3e
guoxuda <guoxuda@micai.asia>
2015-03-20 12:51:18 +0800

    rebase

---
+++ README.md
@@ -0,0 +1,22 @@
+﻿**使用方法**：
+* 解压，编辑menu.js：mutelist是id名，keywords是出现在问题中的关键词，引号、半角逗号用于分割。
+* Chrome：从官方商店中下载，[点击这里](https://chrome.google.com/webstore/detail/zhihu-muter/fdmpgflloeigdkbobkcdlbdlakfbacme)
+* Firefox：安装GreaseMonkey后直接载入menu.js
+
+FAQ：
+
+**为什么？**
+
+在不影响性能的前提下，让网站变得赏心悦目起来。
+
+**临时加了个id/关键词，刷新发现无效？**
+
+保存后如果懒得重启浏览器，[重新载入插件](chrome://extensions/)就好了。 
+
+**为什么会偶尔失效？**
+
+Chrome似乎会随机禁用非官方插件，所以已上传至Chrome商店（最新版），[点击安装](https://chrome.google.com/webstore/detail/zhihu-muter/fdmpgflloeigdkbobkcdlbdlakfbacme)。
+
+**默认的遮蔽列表怎么回事？**
+
+默认那些虽只用于测试和格式示范，但不可否认他们代表了作者立场。
---
+++ mute.js
@@ -0,0 +1,191 @@
+// ==UserScript==
+// @name          Zhihu muter
+// @description   Clean up timeline and question page.
+// @include       http://*.zhihu.com/*
+// @version 	  0.41
+
+// Changelog
+/* 
+0.4.1: mute comments and answers with mouse scroll, added tests and (console.log)s. On/off for relevent question suggestions.
+0.4: add more keywords and ids, efficiency improvements(WAT?), TODOs.
+< 0.4: never mind.
+*/
+
+// TODO
+/*
+* "Settings" function: 
+Easily edit mute_id_list and mute_keywords_list, 
+On/Off button: enable/disable "zh-trendings" in homepage, and "zh-question-related-questions".
+* Performance trace.
+* Combine duplicated codes between each function.
+*/
+
+// TO_MUTE
+/*
+* articles written by ids that on mutelist.
+* "upvoted THESE answers" ->  feed-item folding feed-item-hook feed-item-a combine. 
+*/
+
+// id and keyword list
+var mutelist = ["eric-ann", "yolfilm", "cogito", "gracelu" , "lawrencelry", "nixy", "zi-mo-qi", "Fenng", "Hi-iD", "peng-yu-75-12", "Ratoo", "yang-guo-er-95", "xu-hui-lin", "mu-peng-37", "e6kq"]; 
+var keywords = ["解读", "评价", "优雅", "中国人为什么", "如何正确", "如何成为", "评判", "却不", "体验", "少见但", "如何理解", "冷门但", "什么心理", "什么感觉", "零基础", "如何反驳", "看待", "鲜为人知"];
+
+var top_column_info_hidden = 1;
+var you_may_not_want_to_know = 1;
+var relevent_questions = 1;
+
+// TODO: view and edit keywords/ids list with sample button
+/*
+
+var b = document.createElement("li");
+b.className = "top-nav-noti zu-top-nav-li ";
+b.innerHTML = '<a class="zu-top-nav-link" href="" class="zu-top-nav-link" id="zh-top-nav-count-wrap" role="button">遮蔽</a>';
+document.getElementsByClassName("zu-top-nav-ul zg-clear")[0].appendChild(b);
+
+*/
+
+// 1. Answer
+// 1.1 Answers by specific ids
+function answer_mute(className, tomute) {
+    var elements = document.getElementsByClassName(className);
+    var n = elements.length;
+    var mutelist_length = tomute.length;
+    for (var i = 0; i < n; i++) {
+        var e = elements[i];
+        for (var j = 0; j < mutelist_length; j++) {
+            var muteword = tomute[j];
+            if (e.getElementsByClassName('zm-item-answer-author-info')[0].innerHTML.search(muteword) > -1) {
+                e.style.display = 'none'; 
+                // console.log(e)
+            }
+        }
+    }
+}
+// test
+/*   http://www.zhihu.com/question/26518222   
+var muteword = 'su-chen-chuan'; // no offense
+var elements = document.getElementsByClassName('zm-item-answer');
+var n = elements.length;
+for (var i = 0; i < n; i++) {
+    var answer = elements[i];
+    if (answer.getElementsByClassName('zm-item-answer-author-info')[0].innerHTML.search(muteword) > -1) {
+        //answer.style.display = 'none';
+        //console.log(answer);
+    }
+}
+*/
+
+
+// 1.2 Comments of answer
+function comment_mute(className, tomute) {
+    var elements = document.getElementsByClassName(className);
+    var n = elements.length;
+    for (var i = 0; i < n; i++) {
+        var e = elements[i];
+        for (var j = 0, len = tomute.length; j < len; j++) {
+            var muteword = tomute[j];
+            if (e.getElementsByClassName('zm-comment-hd')[0].innerHTML.search(muteword) > -1) {
+                e.style.display = 'none';
+                //console.log(muteword)
+            }
+        }
+    }
+}
+
+/* test 
+http://www.zhihu.com/question/28256651/answer/40081422 
+click "comments"
+
+var muteword = 'xiao-duan';
+var elements = document.getElementsByClassName('zm-item-comment');
+var n = elements.length;
+for (var i = 0; i < n; i++) {
+    var e = elements[i];
+    if (e.getElementsByClassName('zm-comment-hd')[0].innerHTML.search(muteword) > -1) {
+        e.style.display = 'none';
+        console.log(muteword)
+    }
+}
+*/
+
+
+// 2. Timeline of homepage
+function question_mute(className, tomute) {
+    var elements = document.getElementsByClassName(className);
+    n = elements.length;
+    for (var i = 0; i < n; i++) {
+        var e = elements[i];
+        for (var j = 0, len = tomute.length; j < len; j++) {
+            var muteword = tomute[j];
+            if (e.getElementsByClassName('question_link')[0].innerHTML.search(muteword) > -1) {
+                e.style.display = 'none';
+            }
+        }
+    }
+}
+
+/* test
+no test here
+it just works 
+*/
+
+// Main function
+
+// 0. Get current url to decide what to do
+hrefValue = window.location.href;
+
+// 1. Static methods that do not require listeners
+// 1.1: People you may not want to know
+if (hrefValue == "http://www.zhihu.com/"){
+    if (you_may_not_want_to_know == 1) {
+        var column = document.getElementsByClassName('zh-trendings')[0];
+        column.style.display = 'none';
+        console.log('No user suggestions anymore.')
+    }
+}
+
+// 1.2: Personal info and site log on the top 
+// (hide for bigger -- hiths)
+if (top_column_info_hidden ==1){
+    document.getElementsByClassName('zu-top-link-logo')[0].style.display = 'none';
+    document.getElementsByClassName('name')[0].style.display = 'none';
+    console.log('Site logo and Your Personal Info on top column are hidden.')
+    console.log('For the bigger -- @hiths.')
+}
+
+/* test
+document.getElementsByClassName('zu-top-link-logo')[0].style.display = 'none';
+document.getElementsByClassName('zu-top-nav-userinfo ')[0].style.display = 'none';
+*/
+
+// 2. Methods that are not always used
+// 2.1 Question/Answer page
+if (hrefValue.search('question') > -1) {
+
+    // Relevent question suggestion column
+    if (relevent_questions == 1){
+        document.getElementsByClassName('zh-question-related-questions')[0].style.display = 'none';
+        console.log('No more suggestions on relevent questions.')
+    }
+
+    // Scroll listener to mute answers and comments
+    if (document.addEventListener) {
+        document.addEventListener('scroll', function (event) {
+            comment_mute('zm-item-comment', mutelist);
+            answer_mute('zm-item-answer', mutelist);
+        }, false);
+    }
+}
+
+// 2.2 Timeline
+if (hrefValue.search('question') == -1) {
+    if (document.addEventListener) {
+        document.addEventListener('scroll', function (event) {
+            question_mute('feed-item folding feed-item-hook feed-item-a', keywords);
+            question_mute('feed-item folding feed-item-hook feed-item-p', keywords);
+            question_mute('feed-item folding feed-item-hook feed-item-q', keywords);
+        }, false);
+    }
+}
+
+
