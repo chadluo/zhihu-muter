9e8b25a3279c92c58f8fafa726765b07ed7004a3
ahxxm <ahxxm@users.noreply.github.com>
2016-08-18 21:58:16 +0800

    Update mute.js
    https://www.zhihu.com/question/47925607/answer/108237043

--- mute.js
+++ mute.js
@@ -19,7 +19,7 @@ On/Off button: enable/disable "zh-trendings" in homepage, and "zh-question-relat
 */
 
 // id and keyword list
-var mutelist = ["eric-ann", "yolfilm", "cogito", "gracelu", "lawrencelry", "nixy", "zi-mo-qi", "Fenng", "Hi-iD", "peng-yu-75-12", "Ratoo", "yang-guo-er-95", "xu-hui-lin", "mu-peng-37", "e6kq", "luvletter_"];
+var mutelist = ["eric-ann", "yolfilm", "cogito", "gracelu", "lawrencelry", "nixy", "zi-mo-qi", "Fenng", "Hi-iD", "peng-yu-75-12", "Ratoo", "yang-guo-er-95", "xu-hui-lin", "mu-peng-37", "e6kq", "luvletter_", "ios_dev"];
 var keywords = ["看待", "解读", "评价", "优雅", "如何", "评判", "却不", "体验", "但", "如何理解", "什么", "零基础", "鲜为人知", "特别急", "小众", "你", "有哪些", "现在", "孝", "冷知识", "中国"];
 
 var top_column_info_hidden = 1;
